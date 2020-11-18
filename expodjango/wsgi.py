0e04217583a9c989f1c284b0403e67f73cb94603
unknown <erichris@live.com.mx>
2020-11-18 01:17:43 -0600

    asd

--- expodjango/wsgi.py
+++ expodjango/wsgi.py
@@ -20,9 +20,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expodjango.settings')
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
 
