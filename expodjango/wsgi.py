52c5d6bbbc35a83ea01fee24a96a18fabec25dce
unknown <erichris@live.com.mx>
2020-11-22 00:42:25 -0600

    asd

--- expodjango/wsgi.py
+++ expodjango/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expodjango.settings')
 
 application = get_wsgi_application()
-"""
+
 
 import os
 
@@ -24,4 +24,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expodjango.settings')
 
 application = get_wsgi_application()
+"""
+import os
 
+from django.core.wsgi import get_wsgi_application
+
+from dj_static import Cling
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expodjango.settings')
+
+application = Cling(get_wsgi_application())
